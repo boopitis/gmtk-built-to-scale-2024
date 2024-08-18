93902d0c854b5fe53e52fe35d36d66692f29ffce
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-08-18 12:35:48 -0600

    Create `BaseSpecial` abstract class

---
+++ Assets/Scripts/ScaleSpecials/BaseSpecial.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseSpecial : MonoBehaviour, ISpecial
+{
+    protected enum FireTime
+    {
+        First,
+        Last,
+        Middle,
+        Second,
+        SecondLast
+    }
+    
+    [SerializeField] protected FireTime fireTime;
+    [SerializeField] protected Projectile projectile;
+
+    public abstract void Fire();
+
+    public bool IsQueued(int createScaleLength, int currentNote)
+    {
+        switch (fireTime)
+        {
+            default:
+            case FireTime.First:
+                return currentNote == 0;
+            case FireTime.Last:
+                return currentNote == createScaleLength - 1;
+            case FireTime.Middle:
+                return createScaleLength / 2 == currentNote;
+            case FireTime.Second:
+                return currentNote == 1;
+            case FireTime.SecondLast:
+                return currentNote == createScaleLength - 2;
+        }
+    }
+}
