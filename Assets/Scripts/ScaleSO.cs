8b9706e58fde464b89aba24acd5ad5aeec44c14c
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-08-17 19:11:49 -0600

    `ScaleSO.cs`: Add name field

--- Assets/Scripts/ScaleSO.cs
+++ Assets/Scripts/ScaleSO.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Scale", menuName = "New Scale")]
+[CreateAssetMenu(fileName = "New Scale", menuName = "Scriptable Objects/New Scale")]
 public class ScaleSO : ScriptableObject
 {
     public int[] intervals;
+    public string name;
 }
