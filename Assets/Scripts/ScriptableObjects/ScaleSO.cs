ea40d89706950f42d2e00915aa111fb93e7bc9ca
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-08-18 12:35:10 -0600

    `NoteProjectile`: Separate logic from visuals

--- Assets/Scripts/ScriptableObjects/ScaleSO.cs
+++ Assets/Scripts/ScriptableObjects/ScaleSO.cs
@@ -6,6 +6,7 @@ using UnityEngine.Serialization;
 [CreateAssetMenu(fileName = "New Scale", menuName = "Scriptable Objects/New Scale")]
 public class ScaleSO : ScriptableObject
 {
+    public BaseSpecial special;
     public NoteSO[] noteSOList;
     [FormerlySerializedAs("name")] public string scaleName;
 }
