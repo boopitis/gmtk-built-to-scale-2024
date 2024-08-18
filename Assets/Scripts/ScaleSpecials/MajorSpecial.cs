3e871dad97e7ea683f17b25901d0d70c91564065
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-08-18 12:36:58 -0600

    Create empty `MajorSpecial`, `MelodicMinorSpecial` classes

---
+++ Assets/Scripts/ScaleSpecials/MajorSpecial.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MajorSpecial : BaseSpecial
+{
+    public override void Fire()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/ScaleSpecials/MelodicMinorSpecial.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MelodicMinorSpecial : BaseSpecial
+{
+    public override void Fire()
+    {
+        throw new System.NotImplementedException();
+    }
+}
