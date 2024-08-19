35e987718153eb15d05f650bfaa4ec1579e73a0b
boopitis <stephenravelo31@gmail.com>
2024-08-18 20:24:28 -0600

    Update SpawnDudesExample.cs

--- Assets/Scripts/Enemy AI/SpawnDudesExample.cs
+++ Assets/Scripts/Enemy AI/SpawnDudesExample.cs
@@ -7,6 +7,9 @@ public class SpawnDudesExample : MonoBehaviour
     private GameObject playerGO;
     [SerializeField] private GameObject enemyPrefab;
 
+    [SerializeField] private int enemies = 4;
+    [SerializeField] private float radius = 10f;
+
     private void Awake()
     {
         playerGO = FindObjectOfType<Player>().gameObject;
@@ -14,11 +17,9 @@ public class SpawnDudesExample : MonoBehaviour
 
     private void SpawnDudes()
     {
-        int enemies = 4;
-        int offset = 10;
         for (int i = 0; i < enemies; i++)
         {
-            Instantiate(enemyPrefab, Quaternion.Euler(0, 0, 0 + 360 / enemies * i) * (playerGO.transform.right + new Vector3(offset, 0, 0)), Quaternion.identity);
+            Instantiate(enemyPrefab, Quaternion.Euler(0, 0, 0 + 360 / enemies * i) * (playerGO.transform.right + new Vector3(radius, 0, 0)), Quaternion.identity);
         }
     }
 }
