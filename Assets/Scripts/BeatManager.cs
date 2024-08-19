42016e3b5c5fc82807947738004ada315b37496b
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-08-18 21:43:04 -0600

    `BeatManager`: Add `BeatInterval` for measure timing

--- Assets/Scripts/BeatManager.cs
+++ Assets/Scripts/BeatManager.cs
@@ -20,9 +20,10 @@ public class BeatManager : MonoBehaviour
     [SerializeField] private float bpm;
     [SerializeField] private AudioSource audioSource;
 
-    private BeatInterval beatInterval;
+    private BeatInterval eighthBeatInterval;
+    private BeatInterval measureBeatInterval;
     // Range from 0-15. Stores the current beat that the subdivision is on. (eg. 0 is beat 1, 15 is beat 4+)
-    private int currentSubdivision;
+    private int currentSubdivision, pCurrentSubdivision;
 
     private void Awake()
     {
@@ -30,20 +31,41 @@ public class BeatManager : MonoBehaviour
         
         currentSubdivision = 0;
         
-        beatInterval = new BeatInterval(2); // Set to trigger every eighth note
-        beatInterval.OnTrigger += BeatInterval_OnTrigger;
+        eighthBeatInterval = new BeatInterval(2); // Set to trigger every eighth note
+        measureBeatInterval = new BeatInterval(1.0f/8); // Set to trigger every measure
+
+        eighthBeatInterval.OnTrigger += EighthBeatInterval_OnTrigger;
+        measureBeatInterval.OnTrigger += MeasureBeatInterval_OnTrigger;
     }
 
-    private void BeatInterval_OnTrigger(object sender, EventArgs e)
+    private void MeasureBeatInterval_OnTrigger(object sender, EventArgs e)
     {
-        currentSubdivision = (currentSubdivision + 1) % 16;
+        currentSubdivision = 0;
         OnCurrentSubdivisionChange?.Invoke(this, new OnCurrentSubdivisionChangeEventArgs
         {
             CurrentSubdivision = currentSubdivision
         });
+        pCurrentSubdivision = currentSubdivision;
+    }
+
+    private void EighthBeatInterval_OnTrigger(object sender, EventArgs e)
+    {
+        if (pCurrentSubdivision == 15) return;
+        currentSubdivision++;
+        OnCurrentSubdivisionChange?.Invoke(this, new OnCurrentSubdivisionChangeEventArgs
+        {
+            CurrentSubdivision = currentSubdivision
+        });
+        pCurrentSubdivision = currentSubdivision;
     }
 
     private void Update()
+    {
+        UpdateBeatInterval(eighthBeatInterval);
+        UpdateBeatInterval(measureBeatInterval);
+    }
+
+    private void UpdateBeatInterval(BeatInterval beatInterval)
     {
         float sampledTime = audioSource.timeSamples /
                             (audioSource.clip.frequency * beatInterval.GetIntervalLength(bpm));
