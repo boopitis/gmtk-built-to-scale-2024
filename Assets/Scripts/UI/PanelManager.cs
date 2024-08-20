21e32ef318860ba57198a6dff03921c56dca1f53
boopitis <stephenravelo31@gmail.com>
2024-08-19 20:08:06 -0600

    PauseMenu -> PanelManager

--- Assets/Scripts/UI/PanelManager.cs
+++ Assets/Scripts/UI/PanelManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour
+public class PanelManager : MonoBehaviour
 {
     public event EventHandler StartNextWave;
 
