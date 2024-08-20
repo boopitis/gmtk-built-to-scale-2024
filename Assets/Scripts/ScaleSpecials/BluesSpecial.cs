b684ceb7ed5c33f8282d5224142d898cbe79947b
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-08-20 00:53:20 -0600

    Create Blues scale stuff

---
+++ Assets/Scripts/ScaleSpecials/BluesSpecial.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluesSpecial : BaseSpecial
+{
+    /**
+     * Laser attack
+     */
+    public override void Fire(Transform position, Quaternion rotation)
+    {
+        
+    }
+}
